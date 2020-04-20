958a94682a68792a5edcfc85b9feaadbca832f33
gabyacg29 <g.a.cinquemani@gmail.com>
2020-04-19 22:17:08 -0300

    Actualizacion

---
+++ DH-heroes-GC-refactorizado/controllers/mainControllers.js
@@ -0,0 +1,7 @@
+const mainController = {
+    home: (req, res) => {
+        res.send("Ni Superman, IronMan o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne y hueso que encontrarás en este sitio. Esperamos que ellas y ellos te sirvan como inspiración para poder cumplir tus objetivos. Recuerda: ¡nunca pares de creer en ti!.");
+    }
+};
+
+module.exports = mainController;
